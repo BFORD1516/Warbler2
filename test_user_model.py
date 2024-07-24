293bf10466085060a99c976d4317886cd2636f91
BFORD1516 <151584741+BFORD1516@users.noreply.github.com>
2024-07-24 16:57:17 -0700

    Add files via upload

---
+++ test_user_model.py
@@ -0,0 +1,49 @@
+"""User model tests."""
+
+
+import os
+from unittest import TestCase
+
+from models import db, User, Message, Follows
+
+
+
+os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+
+
+
+
+from app import app
+
+
+
+db.create_all()
+
+
+class UserModelTestCase(TestCase):
+    """Test views for messages."""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+
+        User.query.delete()
+        Message.query.delete()
+        Follows.query.delete()
+
+        self.client = app.test_client()
+
+    def test_user_model(self):
+        """Does basic model work?"""
+
+        u = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add(u)
+        db.session.commit()
+
+       
+        self.assertEqual(len(u.messages), 0)
+        self.assertEqual(len(u.followers), 0)
